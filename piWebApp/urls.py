a5bd5adf0e6c700240ec23a8371252a0209d4b83
Himanshu Mehra <hmehra@usc.edu>
2016-03-19 23:20:36 -0700

    Learn adding home page

--- piWebApp/urls.py
+++ piWebApp/urls.py
@@ -1,3 +1,4 @@
+
 """piWebApp URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -16,7 +17,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from wifiDash import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'', views.measurementList),
 ]
--- wifiDash/views.py
+++ wifiDash/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def measurementList(request):
+    return render(request, 'wifiDash/home.html', {})
