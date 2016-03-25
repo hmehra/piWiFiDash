1356aa090d8b853a6df99b38c05512aebed07e6a
Himanshu Mehra <hmehra@usc.edu>
2016-03-25 00:28:45 -0700

    Django Templates

--- wifiDash/views.py
+++ wifiDash/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import wifiMeasurement
 
 # Create your views here.
 def measurementList(request):
-    return render(request, 'wifiDash/home.html', {})
+    measurements = wifiMeasurement.objects.all().order_by('dateTime')
+    return render(request, 'wifiDash/home.html',
+                  {'measurements': measurements})
